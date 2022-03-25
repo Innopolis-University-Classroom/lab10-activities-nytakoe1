3593d953f8e3e34f1b487d086d01738210b1127a
nytakoe1 <88328194+nytakoe1@users.noreply.github.com>
2022-03-25 17:39:34 +0300

    Update problem2.java

--- src/ru/innopolis/problem2/problem2.java
+++ src/ru/innopolis/problem2/problem2.java
@@ -1,2 +1,53 @@
 // your solution here
 //fix all the errors including package errors
+public class Main {
+
+    public static void main(String[] args) {
+        Burger myBurger = new Burger.Builder().withCheese(true).withPeperone(true).withSize("big").build();
+    }
+}
+
+class Burger{
+    private boolean cheese;
+    private boolean peperone;
+    private boolean  letucci;
+    private boolean  tomato;
+    /*int size;*/
+    String size;
+    public static class Builder{
+        private Burger newBurger;
+
+        public Builder() {
+            newBurger = new Burger();
+        }
+
+        public Builder withCheese(boolean cheese){
+            newBurger.cheese = cheese;
+            return this;
+        }
+
+        public Builder withPeperone(boolean peperone){
+            newBurger.peperone = peperone;
+            return this;
+        }
+
+        public Builder withLetucci(boolean letucci){
+            newBurger.letucci = letucci;
+            return this;
+        }
+
+        public Builder withTomato(boolean tomato){
+            newBurger.tomato = tomato;
+            return this;
+        }
+        public Builder withSize(String size){
+            newBurger.size = size;
+            return this;
+        }
+
+        public Burger build(){
+            return newBurger;
+        }
+    }
+
+}
