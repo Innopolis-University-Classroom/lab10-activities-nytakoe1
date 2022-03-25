c216da4f50a461c56f4133c188ed4ff3c5bb9213
nytakoe1 <88328194+nytakoe1@users.noreply.github.com>
2022-03-25 17:11:50 +0300

    Update problem1.java

--- src/ru/innopolis/problem1/problem1.java
+++ src/ru/innopolis/problem1/problem1.java
@@ -1,2 +1,54 @@
 // your solution here
 //fix all the errors including package errors
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
+ interface shape{
+    void create();
+    double findPerimeter ();
+}
+
+class Rectangle implements shape{
+    double a;
+    double b;
+    @Override
+    public void create() {
+        System.out.println("you have created a triangle ");
+    }
+
+    @Override
+    public double findPerimeter() {
+        return (a+b) * 2;
+    }
+}
+
+class Square implements shape{
+    double a;
+
+    @Override
+    public void create() {
+        System.out.println("you have created a Square ");
+    }
+
+    @Override
+    public double findPerimeter() {
+        return a*4;
+    }
+}
+
+class Circle implements shape{
+    double radious;
+    @Override
+    public void create() {
+        System.out.println("you have created a Circle");
+    }
+
+    @Override
+    public double findPerimeter() {
+
+        return 3.14 * 2 * this.radious;
+    }
+}
